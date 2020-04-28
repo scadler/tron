6a29ae587ac8823e599168ff877b5937ae9b1c28
scadler <scadler@s.sfusd.edu>
2020-04-28 19:17:35 +0000

    new board size is 75*75 and the grid is now shades of blue

--- script.js
+++ script.js
@@ -3,25 +3,35 @@
 // const context = canvas.getContext("2d");
 var player = {
     position: 125,
-    color : "#0000FF",
+    color : "#8888FF",
+    trailColor : "#0000FF",
     direction : 1,
 }
-var i = 0;
- while(i < 2500){
+function createGrid(){
+    var i = 0;
+    console.log("grid")
+    while(i < 5625){
         $("#grid").append(`<div class="cell" id=${i}></div>`);
         i++
- }
- function draw(position, direction ,color){
-     var newPosition = position + direction
-     player.position = newPosition
-     $("#"+newPosition).css("background-color", color);
+    }
+}
+createGrid();
+ function draw(position, direction, color, trailColor){
+    var newPosition = position + direction
+    player.position = newPosition
+    $("#"+position).css("background-color", trailColor);
+    $("#"+newPosition).css("background-color", color);
  }
 
 function game(){
-    draw(player.position, player.direction, player.color)
+    draw(player.position, player.direction, player.color, player.trailColor)
  }
 setInterval(game,100)
- document.addEventListener('keydown', keyPressed)
+function resetGrid(){
+    $("#grid").empty();
+    createGrid();
+}
+document.addEventListener('keydown', keyPressed)
 document.addEventListener('keyup', keyUp)
 function keyPressed(e){
     key = e.key
@@ -32,13 +42,14 @@ function keyPressed(e){
         player.direction = 1
     }
     else if (key == "w") {
-        player.direction = -50
+        player.direction = -75
     }
     else if (key == "s") {
-        player.direction = 50
+        player.direction = 75
     }
     else if(key == " ") {
     e.preventDefault();
+    resetGrid()
     }
   }
 function keyUp(){
