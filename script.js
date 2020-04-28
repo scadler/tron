531112bc2af7c8e7ea3fe987fb51051d640c68e2
scadler <scadler@s.sfusd.edu>
2020-04-28 22:15:41 +0000

    adding comp AI

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //creating canvas & getting context
 // const canvas = document.getElementById("pong");
 // const context = canvas.getContext("2d");
+const status = {
+    game : "inProgress",
+}
 var player = {
     position: 125,
     color : "#8888FF",
@@ -15,37 +18,53 @@ var computer = {
 }
 function createGrid(){
     var i = 0;
-    console.log("grid")
     while(i < 5625){
         $("#grid").append(`<div class="cell" id=${i}></div>`);
+        i++
+    }
+    var i = 0
+    while(i < 5625){
+        if(i<75 || i>5549 || i%75 == 0 || i%75 === 74){
+            $("#"+i).css("background-color","#000070")
+        }
         i++
     }
 }
 createGrid();
  function draw(position, direction, color, trailColor, type){
     var newPosition = position + direction
-    if(type === "player"){
-        player.position = newPosition
-    }else{
-        computer.position = newPosition
+    if($("#"+newPosition).css("background-color") === "rgb(0, 0, 17)"){
+        if(type === "player"){
+            player.position = newPosition
+        }else{
+            computer.position = newPosition
+        }
+        $("#"+position).css("background-color", trailColor);
+        $("#"+newPosition).css("background-color", color);
     }
-    $("#"+position).css("background-color", trailColor);
-    $("#"+newPosition).css("background-color", color);
- }
+    else{
+        status.game = "over"
+    }
+}
 function checkCollisions(position, direction){
-    if((position%75 === 0 && direction === -1) || (position%75 === 74 && direction === 1)){
-        //will later make this code end the game
-        player.position = 1075
-        console.log("A")
-    }
-    else if((position<75 && direction === -75) || (position>5549 && direction === 75)){
-        player.position = 1075
-        console.log("B")
+    if((position%75 === 0 && direction === -1) || (position%75 === 74 && direction === 1) || (position<75 && direction === -75) || (position>5549 && direction === 75)){
+        status.game = "over"
     }
 }
 function computerAI(){
     var newPos = computer.position + computer.direction
-    if($("#"+newPos).css("background-color") !== "rgb(0, 0, 17)"){
+    if(Math.random()-0.9 > 0){
+        var num = Math.random()
+        const PotentialDirection = (num < 0.25) ? -1 : (num < 0.5) ? 1 : (num<0.75) ? 75 : -75;
+        PotentialPosition = computer.position + PotentialDirection;
+        if(PotentialPosition % 75 <= 1 || PotentialPosition % 75 >=72 || PotentialPosition<255 || PotentialPosition>5399){
+            PotentialDirection *= -1
+        }
+        if($("#"+ PotentialPosition).css("background-color") === "rgb(0, 0, 17)"){
+            computer.position = PotentialPosition
+        }
+    }
+    else if($("#"+newPos).css("background-color") !== "rgb(0, 0, 17)"){
         var up = computer.position-75
         var left = computer.position-1
         var down = computer.position+75
@@ -62,15 +81,20 @@ function computerAI(){
         else if($("#"+right).css("background-color") === "rgb(0, 0, 17)"){
             computer.direction = 1
         }
+        else{
+            status.game = "over"
+        }
     }
 }
 function game(){
-    draw(player.position, player.direction, player.color, player.trailColor, "player")
-    draw(computer.position, computer.direction, computer.color, computer.trailColor, "computer")
-    checkCollisions(player.position, player.direction)
-    checkCollisions(computer.position, computer.direction)
-    computerAI()
- }
+    if(status.game==="inProgress"){
+        // draw(player.position, player.direction, player.color, player.trailColor, "player")
+        draw(computer.position, computer.direction, computer.color, computer.trailColor, "computer")
+        checkCollisions(player.position, player.direction)
+        checkCollisions(computer.position, computer.direction)
+        computerAI()
+    }
+}
 setInterval(game,100)
 function resetGrid(){
     $("#grid").empty();
@@ -93,8 +117,9 @@ function keyPressed(e){
         player.direction = 75
     }
     else if(key == " ") {
-    e.preventDefault();
-    resetGrid()
+        e.preventDefault();
+        resetGrid()
+        status.game = "inProgress"
     }
   }
 function keyUp(){
